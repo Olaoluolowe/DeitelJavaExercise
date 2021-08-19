3d26b3f4bed646332f6e52195119e73e97335b21
Olaolu Olowe <olaoluolowe@gmail.com>
2021-08-20 00:08:36 +0100

    Add files via upload
    Palindrome

---
+++ Palindrome.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Palindrome {
+    public static void main(String args[])
+    {
+       /* Scanner s = new Scanner(System.in);
+        System.out.print("Enter 5 digit number: ");
+        String n=s.nextLine();
+        if(n.length()!=5)
+        {
+            System.out.println("The number entered is not five digits long.");
+            return ;
+        }
+        for(int numberEntered=0;numberEntered<2;numberEntered++)
+            if(n.charAt(numberEntered)!=n.charAt(4-numberEntered))
+            {
+                System.out.println("This is not a palindrome");
+                return;
+            }
+        System.out.println("This is a Palindrome");*/
+
+    }
